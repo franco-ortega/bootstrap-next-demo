4955289e18f581860ca36183aa5fb1490b79f16f
Franco Ortega <francopdx@gmail.com>
2022-10-05 12:28:39 -0700

    add "No results" message

--- components/table/Table.jsx
+++ components/table/Table.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Row from './Row';
 
 const Table = ({ data, searchId, exactMatch }) => {
+  const [dataToDisplay, setDataToDisplay] = useState(data);
+  const empty = 'No items matched that search criteria.';
+
+  useEffect(() => {
+    const filteredData = data.filter((item) =>
+      // filter for exact match of ID
+      searchId && exactMatch
+        ? String(item.id) === searchId
+        : // or filter for all IDs that include the number submitted
+        searchId
+        ? String(item.id).includes(searchId)
+        : // otherwise display all data
+          item
+    );
+
+    setDataToDisplay(filteredData);
+  }, [data, searchId, exactMatch]);
+
+  console.log(dataToDisplay);
+
   return (
     <table className='table table-striped w-75'>
       <tbody>
@@ -12,26 +32,28 @@ const Table = ({ data, searchId, exactMatch }) => {
           <th>Color</th>
           <th>Bloop</th>
         </tr>
-        {data
-          .filter((item) =>
-            // filter for exact match of ID
-            searchId && exactMatch
-              ? String(item.id) === searchId
-              : // or filter for all IDs that include the number submitted
-              searchId
-              ? String(item.id).includes(searchId)
-              : // otherwise display all data
-                item
-          )
-          .map((item) => (
-            <Row
-              key={item.id}
-              id={item.id}
-              title={item.title}
-              size={item.size}
-              color={item.color}
-            />
-          ))}
+        {dataToDisplay.length > 0
+          ? dataToDisplay
+              .filter((item) =>
+                // filter for exact match of ID
+                searchId && exactMatch
+                  ? String(item.id) === searchId
+                  : // or filter for all IDs that include the number submitted
+                  searchId
+                  ? String(item.id).includes(searchId)
+                  : // otherwise display all data
+                    item
+              )
+              .map((item) => (
+                <Row
+                  key={item.id}
+                  id={item.id}
+                  title={item.title}
+                  size={item.size}
+                  color={item.color}
+                />
+              ))
+          : empty}
       </tbody>
     </table>
   );
