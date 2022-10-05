58183421c067f97efe60c4c370f204fa17ae2cc8
Franco Ortega <francopdx@gmail.com>
2022-10-04 17:11:53 -0700

    remove Next default styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
+import { useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-import '../styles/globals.css';
+// import '../styles/globals.scss';
+// import '../styles/starter.scss';
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    // import('bootstrap/dist/css/bootstrap.css');
+    // import('../styles/starter.scss');
+    import('bootstrap/dist/js/bootstrap');
+  }, []);
+
   return <Component {...pageProps} />;
 }
 
