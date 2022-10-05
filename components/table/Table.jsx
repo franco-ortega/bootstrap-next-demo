649452c204506d268a597dd7e1531b375f21815e
Franco Ortega <francopdx@gmail.com>
2022-10-05 12:35:25 -0700

    filter data in useEffect and display "No results" if filtered data is empty

--- components/table/Table.jsx
+++ components/table/Table.jsx
@@ -3,7 +3,7 @@ import Row from './Row';
 
 const Table = ({ data, searchId, exactMatch }) => {
   const [dataToDisplay, setDataToDisplay] = useState(data);
-  const empty = 'No items matched that search criteria.';
+  const empty = 'No items matched the search criteria.';
 
   useEffect(() => {
     const filteredData = data.filter((item) =>
@@ -13,49 +13,37 @@ const Table = ({ data, searchId, exactMatch }) => {
         : // or filter for all IDs that include the number submitted
         searchId
         ? String(item.id).includes(searchId)
-        : // otherwise display all data
+        : // otherwise display all data if no searchId is given
           item
     );
 
     setDataToDisplay(filteredData);
   }, [data, searchId, exactMatch]);
 
-  console.log(dataToDisplay);
-
   return (
-    <table className='table table-striped w-75'>
-      <tbody>
-        <tr className='border'>
-          <th>ID</th>
-          <th>Title</th>
-          <th>Size</th>
-          <th>Color</th>
-          <th>Bloop</th>
-        </tr>
-        {dataToDisplay.length > 0
-          ? dataToDisplay
-              .filter((item) =>
-                // filter for exact match of ID
-                searchId && exactMatch
-                  ? String(item.id) === searchId
-                  : // or filter for all IDs that include the number submitted
-                  searchId
-                  ? String(item.id).includes(searchId)
-                  : // otherwise display all data
-                    item
-              )
-              .map((item) => (
-                <Row
-                  key={item.id}
-                  id={item.id}
-                  title={item.title}
-                  size={item.size}
-                  color={item.color}
-                />
-              ))
-          : empty}
-      </tbody>
-    </table>
+    <>
+      <table className='table table-striped w-75'>
+        <tbody>
+          <tr className='border'>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Size</th>
+            <th>Color</th>
+            <th>Bloop</th>
+          </tr>
+          {dataToDisplay.map((item) => (
+            <Row
+              key={item.id}
+              id={item.id}
+              title={item.title}
+              size={item.size}
+              color={item.color}
+            />
+          ))}
+        </tbody>
+      </table>
+      {!dataToDisplay.length && empty}
+    </>
   );
 };
 
