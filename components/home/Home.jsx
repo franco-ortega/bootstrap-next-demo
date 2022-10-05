2c9606b193a2feb4f9c18bb2bfe999ecfc690cc3
Franco Ortega <francopdx@gmail.com>
2022-10-04 18:35:36 -0700

    sort columns in ascending order

--- components/home/Home.jsx
+++ components/home/Home.jsx
@@ -2,37 +2,100 @@ import React, { useEffect, useState } from 'react';
 import ItemList from '../items/ItemList';
 import Table from '../table/Table';
 
+const items = [
+  {
+    title: 'Best Book',
+    size: 'Medium',
+    color: 'Green',
+    id: 1
+  },
+  {
+    title: 'Worst Book',
+    size: 'Large',
+    color: 'Orange',
+    id: 2
+  },
+  {
+    title: 'Rad Book',
+    size: 'Small',
+    color: 'Pink',
+    id: 3
+  }
+];
+
 const Home = () => {
   const [data, setData] = useState([]);
+  const [sortType, setSortType] = useState('');
 
   useEffect(() => {
-    setData([
-      {
-        title: 'The Best Book',
-        size: 'Medium',
-        color: 'Green',
-        id: 1
-      },
-      {
-        title: 'The Worst Book',
-        size: 'Large',
-        color: 'Orange',
-        id: 2
-      },
-      {
-        title: 'The Rad Book',
-        size: 'Small',
-        color: 'Pink',
-        id: 3
-      }
-    ]);
-  }, []);
+    setData(items);
+
+    const sortArray = (type) => {
+      const types = {
+        id: 'id',
+        title: 'title',
+        color: 'color',
+        size: 'size'
+      };
+
+      const sortProperty = types[type];
+
+      const sorted = [...items].sort((a, b) => {
+        // return b[sortProperty] - a[sortProperty];
+
+        if (a[sortProperty] < b[sortProperty]) {
+          return -1;
+        }
+        if (a[sortProperty] > b[sortProperty]) {
+          return 1;
+        }
+        // a must be equal to b
+        return 0;
+      });
+
+      setData(sorted);
+    };
+
+    sortArray(sortType);
+  }, [sortType]);
+
+  const onIdSortClick = () => {
+    setSortType('id');
+  };
+
+  const onTitleSortClick = () => {
+    setSortType('title');
+  };
+
+  const onSizeSortClick = () => {
+    setSortType('size');
+  };
+
+  const onColorSortClick = () => {
+    setSortType('Color');
+  };
+
+  console.log(data[0]);
 
   return (
     <div>
       <h2>Items</h2>
-      <ItemList items={data} />
-      <Table data={data} />
+      {/* <ItemList items={data} /> */}
+      <div className='d-flex gap-5'>
+        <button className='btn btn-primary' onClick={onIdSortClick}>
+          Sort by ID
+        </button>
+        <button className='btn btn-primary' onClick={onTitleSortClick}>
+          Sort by Title
+        </button>
+        <button className='btn btn-primary' onClick={onSizeSortClick}>
+          Sort by Size
+        </button>
+        <button className='btn btn-primary' onClick={onColorSortClick}>
+          Sort by Color
+        </button>
+      </div>
+      {data.length > 0 && <Table data={data} />}
     </div>
   );
 };
--- components/table/Table.jsx
+++ components/table/Table.jsx
@@ -2,24 +2,27 @@ import React from 'react';
 import Row from './Row';
 
 const Table = ({ data }) => {
+  console.log(data[0]);
   return (
-    <table className='table table-striped w-50'>
-      <tr className='border'>
-        <th>ID</th>
-        <th>Title</th>
-        <th>Size</th>
-        <th>Color</th>
-        <th>Bloop</th>
-      </tr>
-      {data.map((item) => (
-        <Row
-          key={item.id}
-          id={item.id}
-          title={item.title}
-          size={item.size}
-          color={item.color}
-        />
-      ))}
+    <table className='table table-striped w-75'>
+      <tbody>
+        <tr className='border'>
+          <th>ID</th>
+          <th>Title</th>
+          <th>Size</th>
+          <th>Color</th>
+          <th>Bloop</th>
+        </tr>
+        {data.map((item) => (
+          <Row
+            key={item.id}
+            id={item.id}
+            title={item.title}
+            size={item.size}
+            color={item.color}
+          />
+        ))}
+      </tbody>
     </table>
   );
 };
